fbc8a3e4ebff2fac8ab7ae05dec4ab9bd6c51144
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-19 21:51:52 +0000

    Add a signup form

---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import styles from "../../styles/SignUpForm.module.css";
+// import { Link } from "react-router-dom";
+
+import {
+  Form,
+  Button,
+  Card,
+  Col,
+  Row,
+  Container,
+  Alert,
+} from "react-bootstrap";
+import axios from "axios";
+
+const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    username: "",
+    password1: "",
+    password2: "",
+  });
+  const { name, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setSignUpData(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/registration/", signUpData);
+      history.push("/signin");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+  
+  
+
+
+  return (
+    <Row className={styles.Row}>
+      <Col>
+        <Container>
+          <Card className="text-center d-flex justify-content-center mx-auto">
+            <Card.Header>Sign up</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group controlId="formBasicName">
+                    <Form.Label>Name:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Username"
+                      name="username"
+                      value={name}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                  {errors.username?.map((message, idx) => (
+                    <Alert variant="warning" key={idx}>
+                      {message}
+                    </Alert>
+                  ))}
+
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      name="password1"
+                      value={password1}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                  {errors.password1?.map((message, idx) => (
+                    <Alert variant="warning" key={idx}>
+                      {message}
+                    </Alert>
+                  ))}
+
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Confirm password:</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Confirm password"
+                      name="password2"
+                      value={password2}
+                      onChange={handleChange}
+                    />
+                  </Form.Group>
+                  {errors.password2?.map((message, idx) => (
+                    <Alert variant="warning" key={idx}>
+                      {message}
+                    </Alert>
+                  ))}
+                  <Button variant="danger" type="submit">
+                    Sign up
+                  </Button>
+                  {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+                </Form>
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted">
+              <Link className={styles.Link} to="/signin">
+                Already have an account? <span>Log in</span>
+              </Link>
+            </Card.Footer>
+          </Card>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUpForm;
