ed367fb56e6bbbf1571fd97896fd5f9e429693b5
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-19 21:53:36 +0000

    Add signupform to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Innoevent</h1>} />
           <Route exact path="/events" render={() => <h1>Events</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <h1>Page not found!</h1>} />
         </Switch>
       </Container>
