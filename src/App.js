4687d1d53f4f2520b0cff4bc0a389f46e1bf61af
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-20 10:30:57 +0000

    Add handlesignout to log out & add logged in links

--- src/App.js
+++ src/App.js
@@ -6,21 +6,20 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 
-
 function App() {
   return (
-        <div className={styles.App}>
-          <NavBar />
-          <Container className={styles.Main}>
-            <Switch>
-              <Route exact path="/" render={() => <h1>Innoevent</h1>} />
-              <Route exact path="/events" render={() => <h1>Events</h1>} />
-              <Route exact path="/signin" render={() => <SignInForm />} />
-              <Route exact path="/signup" render={() => <SignUpForm />} />
-              <Route render={() => <h1>Page not found!</h1>} />
-            </Switch>
-          </Container>
-        </div>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Innoevent</h1>} />
+          <Route exact path="/events" render={() => <h1>Events</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <h1>Page not found!</h1>} />
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,41 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const setCurrentUser = useSetCurrentUser();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addEventIcon = <NavLink to="/events/create">Create event</NavLink>;
+
+  const loggedInIcons = (
+    <>
+      <NavLink to={`/profiles/${currentUser?.profile_id}`}>
+        {currentUser?.username}
+      </NavLink>
+      <NavLink to="/" onClick={handleSignOut}>
+        Sign out
+      </NavLink>
+    </>
+  );
   const loggedOutIcons = (
     <>
-      <NavLink to="/signin">Sign In</NavLink>
-      <NavLink to="/signup">Sign Up</NavLink>
+      <NavLink to="/signin">Sign in</NavLink>
+      <NavLink to="/signup">Sign up</NavLink>
     </>
   );
 
@@ -31,6 +57,7 @@ const NavBar = () => {
             <Nav className="ml-auto">
               <NavLink to="/">Home</NavLink>
               <NavLink to="/events">Events</NavLink>
+              {currentUser && addEventIcon}
               {currentUser ? loggedInIcons : loggedOutIcons}
             </Nav>
           </Navbar.Collapse>
