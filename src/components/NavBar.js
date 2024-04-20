5a2ca7bf625d38d44bc85279da90dedf313c2e59
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-20 08:25:31 +0000

    Add current user & logged in and out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink to="/signin">Sign In</NavLink>
+      <NavLink to="/signup">Sign Up</NavLink>
+    </>
+  );
+
   return (
     <div>
       <Container>
@@ -21,8 +31,7 @@ const NavBar = () => {
             <Nav className="ml-auto">
               <NavLink to="/">Home</NavLink>
               <NavLink to="/events">Events</NavLink>
-              <NavLink to="/signin">Sign In</NavLink>
-              <NavLink to="/signup">Sign Up</NavLink>
+              {currentUser ? loggedInIcons : loggedOutIcons}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
