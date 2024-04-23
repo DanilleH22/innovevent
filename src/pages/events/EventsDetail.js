8cc68ff89123a6dd069b5d022d922391813e2c69
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-23 09:16:16 +0000

    Remove code for Events and move to EventDetails

--- src/pages/events/EventsDetail.js
+++ src/pages/events/EventsDetail.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-import Events from "./Events";
+import EventDetails from "./EventDetails";
 
 function EventsDetail() {
   const { id } = useParams();
@@ -27,7 +27,7 @@ function EventsDetail() {
   return (
     <Row className="h100 d-flex justify-content-center">
       <Col className="d-flex justify-content-center">
-        <Events {...eventDetails.results[0]} setEventDetails={setEventDetails} EventDetails />
+        <EventDetails {...eventDetails.results[0]} setEventDetails={setEventDetails} EventDetails />
       </Col>
     </Row>
   );
