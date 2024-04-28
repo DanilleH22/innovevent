f242b3d80c5b1634700bb2c1c715dd3dbdb3b74b
Danille Hamilton <danilleham@hotmail.co.uk>
2024-04-28 18:57:34 +0000

    Refactor: Change styling of carousel image

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Row, Col, Container, Card, Button, Carousel } from "react-bootstrap";
-import Network from "../../assets/network.png"
-import Networking2 from "../../assets/networking2.png"
-import Networking3 from "../../assets/networking3.png"
+import {
+  Row,
+  Col,
+  Container,
+  Card,
+  Button,
+  Carousel,
+  Link,
+} from "react-bootstrap";
+import Network from "../../assets/network.png";
+import Networking2 from "../../assets/networking2.png";
+import Networking3 from "../../assets/networking3.png";
+import styles from "../../App.module.css";
 
 const Homepage = () => {
   return (
@@ -13,25 +22,23 @@ const Homepage = () => {
           <h5>Bringing events to you!</h5>
         </Col>
       </Row>
-      <Row className="pb-4 h-50">
+      <Row className="pb-4">
         <Col className="h-50">
           <Carousel>
             <Carousel.Item>
               <img
-                className="d-block w-100"
+                className={`d-block w-100 ${styles.carouselImage}`}
                 src={Network}
                 alt="Network conference"
               />
               <Carousel.Caption>
                 <h3>Learn</h3>
-                <p>
-                  Learn from professionals in your field
-                </p>
+                <p>Learn from professionals in your field</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img
-                className="d-block w-100"
+                className={`d-block w-100 ${styles.carouselImage}`}
                 src={Networking2}
                 alt="Second slide"
               />
@@ -43,22 +50,20 @@ const Homepage = () => {
             </Carousel.Item>
             <Carousel.Item>
               <img
-                className="d-block w-100"
+                className={`d-block w-100 ${styles.carouselImage}`}
                 src={Networking3}
                 alt="Third slide"
               />
 
               <Carousel.Caption>
                 <h3>Speak</h3>
-                <p>
-                  Speak about 
-                </p>
+                <p>Speak about</p>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
         </Col>
       </Row>
-      <Row>
+      <Row className="text-center">
         <Col md={4} className="mb-4">
           <Card className="h-100">
             <Card.Body>
@@ -66,18 +71,6 @@ const Homepage = () => {
               <Card.Text>
                 Discover the benefits of joining our tech events and enhance
                 your skills, network, and career opportunities.
-              </Card.Text>
-              <Button variant="danger">Learn More</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4} className="mb-4">
-          <Card className="h-100">
-            <Card.Body>
-              <Card.Title>Benefits from Tech Events</Card.Title>
-              <Card.Text>
-                Engage with experts and thought leaders in technology. Gain
-                insights and exposure to new technologies and trends.
               </Card.Text>
             </Card.Body>
           </Card>
@@ -90,7 +83,24 @@ const Homepage = () => {
                 Stay updated with the latest events and workshops happening in
                 the tech industry around you.
               </Card.Text>
-              <Button variant="danger">View Events</Button>
+              <Button
+                variant="danger"
+                as={Link}
+                to={`/events/`}
+              >
+                View Events
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4} className="mb-4">
+          <Card className="h-100">
+            <Card.Body>
+              <Card.Title>Benefits from Tech Events</Card.Title>
+              <Card.Text>
+                Engage with experts and thought leaders in technology. Gain
+                insights and exposure to new technologies and trends.
+              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
